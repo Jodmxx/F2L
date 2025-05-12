f65e037de576d99cc7c6c0446418ecc6f79fd12c
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2025-05-12 11:28:40 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,11 +11,11 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv("API_ID", 29256711))
     API_HASH = str(getenv('API_HASH', "e1e7f435524bc0c756783949cad78fcd"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "6185171594:AAGvbbvFlejRYaWbiN_S6fsOvXbUOvTsnJ4"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "6875581535:AAFIQnFPm2KnHVwsO1_-ebb4iPInTZfqE0c"))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001750626831"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1002638667541"))
     PORT = int(getenv('PORT', ))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME' "mrx235"))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', "fil2l-a2502557ebe7"))
+        APP_NAME = str(getenv('APP_NAME', "jodfl2-3934e9c09c15"))
    
     else:
         ON_HEROKU = False
